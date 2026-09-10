741055295e26227f0f17274a941048d24d4e75b3
maksi <maksi@HONORMAGICBOOK>
2026-09-10 10:35:02 +0300

    Добавьте файлы проекта.

---
+++ 3ISIP124_Shchegolev/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _3ISIP124_Shchegolev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
